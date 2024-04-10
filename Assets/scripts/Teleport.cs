f648b5e80d70c6186f7f08637c37ef0a7742df89
Colton <69281405+Carmel70@users.noreply.github.com>
2024-04-10 11:12:07 -0500

    wyeshehgbeg

--- Assets/scripts/Teleport.cs
+++ Assets/scripts/Teleport.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Runtime.CompilerServices;
 
 public class Teleport : MonoBehaviour
 {
+    public int x;
+    public int y;
+    public int z;
+
+    public GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,13 @@ public class Teleport : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Player.transform.position = new Vector3(x, y, z);
+        }
     }
 }
