8bfba9d39312856cb2f8c4d75a83b81c9644b1e5
Colton <69281405+Carmel70@users.noreply.github.com>
2024-04-10 22:41:36 -0500

    big changes

--- Assets/scripts/Teleport.cs
+++ Assets/scripts/Teleport.cs
@@ -1,9 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Teleport : MonoBehaviour
 {
+    public GameObject Player;
+    public GameObject OldYuno;
+    public GameObject NewYuno;
+    public GameObject OldCam;
+    public GameObject OldVCam;
+    public GameObject OldCamCol;
+    public GameObject NewCam;
+    public GameObject NewVCam;
+    public GameObject NewCamCol;
+
+
+    public GameObject oldMusic;
+    public GameObject newMusic;
+   
+
+    public float x;
+    public float y;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +32,26 @@ public class Teleport : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Player.transform.position = new Vector2(x, y);
+  
+        oldMusic.SetActive(false);
+
+        OldCam.SetActive(false);
+        OldVCam.SetActive(false);
+        OldCamCol.SetActive(false);
+
+       
+
+        NewCam.SetActive(true);
+        NewVCam.SetActive(true);
+        NewCamCol.SetActive(true);
+
+        newMusic.SetActive(true);
+    }
+
 }
